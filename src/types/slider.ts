6a4727b460d26f82303e26490684763e1e6c8a55
tora0621 <kotaro19860621@icloud.com>
2022-04-27 10:34:33 +0900

    install react-slick,create slider

---
+++ src/types/slider.ts
@@ -0,0 +1,14 @@
+export type SliderType = {
+  data: Data;
+
+  // img: string;
+  // title: string;
+};
+// export type SliderType = {
+//   data: Data;
+
+//   // img: string;
+//   // title: string;
+// };
+
+type Data = [{ img: string; title: string }];
