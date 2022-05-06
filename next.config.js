110ac1bcfa2b4a5dfd2709d648fe8d4b2e5fc831
tora0621 <kotaro19860621@icloud.com>
2022-05-06 17:52:55 +0900

    install bundleanalyze

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
 
 const path = require('path');
 
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['placekitten.com'],
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
@@ -15,4 +18,25 @@ module.exports = {
   images: {
     domains: ['placekitten.com'],
   },
+  withBundleAnalyzer,
 };
+
+module.exports = nextConfig;
+
+// module.exports = {
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, 'styles')],
+//   },
+//   //外部から画像を読み込む場合ココにドメインを指定,ref:https://nextjs.org/docs/basic-features/image-optimization#domains
+//   images: {
+//     domains: ['placekitten.com'],
+//   },
+// };
+
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// });
+
+// module.exports = withBundleAnalyzer({
+//   // 他の設定
+// });
